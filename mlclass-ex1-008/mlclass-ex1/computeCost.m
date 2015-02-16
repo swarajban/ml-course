c80714fe8a5a8a4aa25b97cb040465724958769b
swaraj <swaraj@expectlabs.com>
2015-02-15 18:31:15 -0800

    Finished warm up exercise, initial plot, and cost function for initial thetas

--- mlclass-ex1-008/mlclass-ex1/computeCost.m
+++ mlclass-ex1-008/mlclass-ex1/computeCost.m
@@ -13,9 +13,11 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+prediction = X * theta;
+cost = prediction - y;
+squaredCost = cost .^ 2;
+sumSquaredCost = sum(squaredCost);
+J = (1 / (2 * m)) * sumSquaredCost;
 
 % =========================================================================
 
