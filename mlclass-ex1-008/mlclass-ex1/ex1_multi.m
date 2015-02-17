874c1c51d53840d59aee4464d5d03329416be350
swaraj <swaraj@expectlabs.com>
2015-02-17 02:17:58 -0800

    implemented normal eqn

--- mlclass-ex1-008/mlclass-ex1/ex1_multi.m
+++ mlclass-ex1-008/mlclass-ex1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 0.1;
+num_iters = 2000;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -97,7 +97,7 @@ theta = zeros(3, 1);
 
 % Plot the convergence graph
 figure;
-plot(1:50, J_history(1:50), '-b', 'LineWidth', 2);
+plot(1:length(J_history), J_history, '-b', 'LineWidth', 2);
 % hold on;
 % plot(1:50, J_s(1:50), '-r', 'LineWidth', 2);
 % plot(1:50, J_b(1:50), '-k', 'LineWidth', 2);
@@ -163,7 +163,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+test = [1 1650 3];
+price = test * theta; % You should change this
 
 
 % ============================================================
