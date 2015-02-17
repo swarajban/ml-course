40822cafdbf98dd3b999a7587de706f772b25fba
swaraj <swaraj@expectlabs.com>
2015-02-17 01:51:57 -0800

    implemented vectorized gradient descent

--- mlclass-ex1-008/mlclass-ex1/computeCostMulti.m
+++ mlclass-ex1-008/mlclass-ex1/computeCostMulti.m
@@ -14,7 +14,11 @@ J = 0;
 %               You should set J to the cost.
 
 
-
+prediction = X * theta;
+cost = prediction - y;
+squaredCost = cost .^ 2;
+sumSquaredCost = sum(squaredCost);
+J = (1 / (2 * m)) * sumSquaredCost;
 
 
 % =========================================================================
--- mlclass-ex1-008/mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1-008/mlclass-ex1/gradientDescentMulti.m
@@ -5,6 +5,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = size(X, 2); % number of features
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
@@ -16,16 +17,15 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+    delta = zeros(n, 1);
+    for i = 1 : m,
+        row = X(i, :);
+        prediction = row * theta;
+        cost = prediction - y(i);
+        delta += cost * row';
+    end;
+    delta *= 1 / m;
+    theta = theta - (alpha * delta);
 
     % ============================================================
 
